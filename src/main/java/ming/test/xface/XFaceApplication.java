0c73aa6b201f6acc97aa215e2ff5f5fffaefd33d
XiaoMing <2784416301@qq.com>
2024-05-31 20:06:15 +0800

    first commit

---
+++ src/main/java/ming/test/xface/XFaceApplication.java
@@ -0,0 +1,13 @@
+package ming.test.xface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XFaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XFaceApplication.class, args);
+    }
+
+}
