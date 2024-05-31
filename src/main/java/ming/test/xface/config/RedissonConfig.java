3e4df802a13952d5f5f61ab3a073d860d134912a
XiaoMing <2784416301@qq.com>
2024-05-31 22:36:38 +0800

    Redis 配置以及使用

---
+++ src/main/java/ming/test/xface/config/RedissonConfig.java
@@ -0,0 +1,34 @@
+package ming.test.xface.config;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "redis")
+@Data
+@Slf4j
+public class RedissonConfig {
+
+    private String ip;
+    private String port;
+    private String password;
+    private Integer database;
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        System.out.println("redis ip: " + ip + ", port: " + port);
+        String redisAddress = String.format("redis://%s:%s", ip, port);
+        config.useSingleServer().setAddress(redisAddress).setPassword(password).setDatabase(database);
+//        Config config = Config.fromYAML(new File("config-file.yaml"));
+        RedissonClient redisson = Redisson.create(config);
+        return redisson;
+    }
+
+}
