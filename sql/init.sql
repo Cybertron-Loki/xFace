30e3368173287b4327fe6150eb297a1f6290270f
cybertron-loki <944980390@qq.com>
2024-06-05 11:50:10 +0800

    加了点东西

--- sql/init.sql
+++ sql/init.sql
@@ -50,6 +50,7 @@ CREATE TABLE `Group`
     `status`     int(11)      NOT NULL DEFAULT 0 COMMENT '群组状态 0-正常 1-禁用等',
     `createTime` DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     `updateTime` DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `type`       INT(11)      NOT NULL default 0 COMMENT '族群类别，默认0-朋友群，1-家庭群，2-同事群 ...',
     PRIMARY KEY (`id`),
     FOREIGN KEY (`creatorId`) REFERENCES `User` (`id`) ON DELETE CASCADE
 ) ENGINE = InnoDB
@@ -65,6 +66,7 @@ CREATE TABLE `GroupMember`
     `status`     int(11)  NOT NULL DEFAULT 0 COMMENT '群组状态 0-正常 1-禁用等',
     `createTime` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     `updateTime` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `nickName`   VARCHAR(255) COMMENT '群内昵称',
     PRIMARY KEY (`groupId`, `userId`),
     FOREIGN KEY (`groupId`) REFERENCES `Group` (`id`) ON DELETE CASCADE,
     FOREIGN KEY (`userId`) REFERENCES `User` (`id`) ON DELETE CASCADE
@@ -97,12 +99,13 @@ CREATE TABLE `diary`
 (
     `id`         INT(11)      NOT NULL AUTO_INCREMENT COMMENT '日记id',
     `userId`     INT(11)      NOT NULL COMMENT '用户id',
-    -- 是否需要创建类别表 进行外键相连
+    -- 是否需要创建类别表 进行外键相连   我觉得不用，这个就行
     `type`       VARCHAR(255) NOT NULL COMMENT '日记类型',
     `title`      VARCHAR(255) NOT NULL COMMENT '日记标题',
     `content`    TEXT         NOT NULL COMMENT '日记内容',
     `createTime` DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     `updateTime` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `visible`    VARCHAR(255) NOT NULL COMMENT '隐私：先定为公开可见或者仅群内可见或者仅自己可见',
     PRIMARY KEY (`id`),
     FOREIGN KEY (`userId`) REFERENCES `User` (`id`)
 ) ENGINE = InnoDB
@@ -111,9 +114,9 @@ CREATE TABLE `diary`
 
 DROP TABLE IF EXISTS `comment`;
 
-CREATE TABLE `comment`
+CREATE TABLE  `comment`
 (
-    `id`         INT(11) NOT NULL AUTO_INCREMENT COMMENT '评论id',
+    `id`         INT(11)   NOT NULL AUTO_INCREMENT COMMENT '评论id',
     `userId`     INT(11) NOT NULL COMMENT '用户id',
     `diaryId`    INT(11) NOT NULL COMMENT '日记id',
     `parentId`   INT(11)  DEFAULT -1 COMMENT '父评论id',
@@ -124,3 +127,38 @@ CREATE TABLE `comment`
 ) ENGINE = InnoDB
   AUTO_INCREMENT = 2
   DEFAULT CHARSET = utf8 COMMENT = '评论表';
+
+
+DROP TABLE IF EXISTS  `blog`;
+
+CREATE TABLE `blog`
+(
+    `id`         INT(11) NOT NULL AUTO_INCREMENT COMMENT '个人发表的避雷/种草/旅游经验帖子',
+    `userId`     INT(11) NOT NULL COMMENT '用户id',
+    `commentId`  INT(11) NOT NULL  COMMENT '评论id',
+    `createTime` DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `updateTime` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `type`       VARCHAR(255) NOT NULL COMMENT 'blog类型',
+    `visible`    VARCHAR(255) NOT NULL COMMENT '隐私：先定为公开可见或者仅群内可见或者仅自己可见',
+    `content`    TEXT  NOT NULL COMMENT 'blog内容',
+    `brand_id`   INT(11) NOT NULL COMMENT  '关联商家id（可不填）',
+    `location`   VARCHAR(255) COMMENT '所在位置',
+     PRIMARY KEY (`id`),
+     FOREIGN KEY (`brand_id`) REFERENCES `brand` (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 2
+  DEFAULT CHARSET = utf8 COMMENT = '帖子表';
+
+DROP TABLE IF EXISTS `brand`;
+CREATE TABLE `brand`
+(
+    `id`          INT(11) NOT NULL AUTO_INCREMENT COMMENT '商家id',
+    `name`        VARCHAR(255) NOT NULL COMMENT '商家名/品牌名',
+    `createTime`  DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `updateTime`  DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `location`    VARCHAR(255) NOT NULL COMMENT '商家名/品牌位置',
+    `score`       INT(5)    NOT NULL COMMENT '商家评分',
+     PRIMARY KEY (`id`)
+)ENGINE = InnoDB
+  AUTO_INCREMENT = 2
+  DEFAULT CHARSET = utf8 COMMENT = '商家/品牌表';
