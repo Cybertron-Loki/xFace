5a80f7d99b40beb72aefd3433b6dd57ccf316584
Xiaoming <2784416301@qq.com>
2024-06-07 00:15:33 +0800

    分页插件

--- src/main/java/ming/test/xface/dao/UserMapper.java
+++ src/main/java/ming/test/xface/dao/UserMapper.java
@@ -3,6 +3,9 @@ package ming.test.xface.dao;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import ming.test.xface.enity.pojo.User;
 import ming.test.xface.enity.vo.UserVO;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 /**
  * @author XiaoMing
@@ -11,7 +14,21 @@ import ming.test.xface.enity.vo.UserVO;
  * @Entity ming.test.xface.enity.pojo.User
  */
 public interface UserMapper extends BaseMapper<User> {
+    /**
+     * 通过唯一标识获取用户信息
+     *
+     * @param id 用户唯一标识
+     * @return
+     */
     UserVO getUserById(Integer id);
+
+    /**
+     * 测试获取所有用户信息
+     *
+     * @return
+     */
+    @Select("SELECT * FROM User")
+    List<UserVO> allUserInfo();
 }
 
 
