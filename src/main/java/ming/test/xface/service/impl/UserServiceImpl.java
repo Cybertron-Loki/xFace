c8dbc7231e99cd280d23f6543a6425e9215c2e59
Xiaoming <2784416301@qq.com>
2024-06-09 15:53:07 +0800

    格式化代码

--- src/main/java/ming/test/xface/service/impl/UserServiceImpl.java
+++ src/main/java/ming/test/xface/service/impl/UserServiceImpl.java
@@ -3,23 +3,19 @@ package ming.test.xface.service.impl;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.bean.copier.CopyOptions;
 import cn.hutool.core.util.RandomUtil;
-
 import ming.test.xface.dao.UserMapper;
 import ming.test.xface.enity.dto.Result;
 import ming.test.xface.enity.dto.UserLoginDTO;
 import ming.test.xface.enity.pojo.User;
 import ming.test.xface.enity.vo.UserVO;
 import ming.test.xface.service.UserService;
-
 import ming.test.xface.utils.RegexUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 
-
 import javax.annotation.Resource;
-import javax.servlet.http.HttpSession;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
@@ -34,7 +30,7 @@ import static ming.test.xface.common.RedisConstant.*;
  * @createDate 2024-06-05 20:48:03
  */
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
 
     @Resource
@@ -45,6 +41,7 @@ public class UserServiceImpl implements UserService{
 
     /**
      * 短信登陆
+     *
      * @param userLoginDTO
      * @return
      */
@@ -52,52 +49,48 @@ public class UserServiceImpl implements UserService{
     public Result loginByMes(UserLoginDTO userLoginDTO) {
         String phonenum = userLoginDTO.getPhoneNum();
         boolean phoneInvalid = RegexUtils.isPhoneInvalid(phonenum);// 二次检验。防止改手机号登录；或者前端写死不能改
-        if(phoneInvalid==true)
-            return Result.fail("手机号不对");
+        if (phoneInvalid == true) return Result.fail("手机号不对");
 
 
         String code = stringRedisTemplate.opsForValue().get(Login_Code_Key + phonenum);
-        if(code==null||!code.equals(userLoginDTO.getCode())){
+        if (code == null || !code.equals(userLoginDTO.getCode())) {
             return Result.fail("验证码不对，登陆失败");
         }
         //是否注册过
-        User user=userMapper.getByPhone(phonenum);
-        if(user==null){
-            user=createUser(userLoginDTO);
+        User user = userMapper.getByPhone(phonenum);
+        if (user == null) {
+            user = createUser(userLoginDTO);
         }
 
 
-        String token= UUID.randomUUID().toString();  //给前端发token
-        String tokenKey=Login_Token_Key+token;       //后端以hashmap形式存储到缓存
+        String token = UUID.randomUUID().toString();  //给前端发token
+        String tokenKey = Login_Token_Key + token;       //后端以hashmap形式存储到缓存
 
         UserVO userVO = BeanUtil.copyProperties(user, UserVO.class);
-        Map<String, Object> userMap = BeanUtil.beanToMap(userVO, new HashMap<>()
-                , CopyOptions.create().setIgnoreNullValue(true)
-                        .setFieldValueEditor(
-                                (name, value) -> {
-                                    if (value != null) {
-                                        return value.toString();
-                                    } else {
-                                        return null; // 或者你想要的默认值
-                                    }
-                                }
-                        ));
+        Map<String, Object> userMap = BeanUtil.beanToMap(userVO, new HashMap<>(), CopyOptions.create().setIgnoreNullValue(true).setFieldValueEditor((name, value) -> {
+            if (value != null) {
+                return value.toString();
+            } else {
+                return null; // 或者你想要的默认值
+            }
+        }));
 
-        stringRedisTemplate.opsForHash().putAll(tokenKey,userMap);
-        stringRedisTemplate.expire(tokenKey,Login_User_TTL,TimeUnit.MINUTES);
+        stringRedisTemplate.opsForHash().putAll(tokenKey, userMap);
+        stringRedisTemplate.expire(tokenKey, Login_User_TTL, TimeUnit.MINUTES);
         return Result.success(token);
     }
 
     /**
      * 第一次注册
+     *
      * @param
      */
     private User createUser(UserLoginDTO userLoginDTO) {
-        User user=new User();
-        BeanUtil.copyProperties(userLoginDTO,user);
+        User user = new User();
+        BeanUtil.copyProperties(userLoginDTO, user);
         String encryptPassword = DigestUtils.md5DigestAsHex((SALT + userLoginDTO.getPassword()).getBytes());
         user.setPassword(encryptPassword);
-        String userName= UserName_PREFIX+RandomUtil.randomString(10);
+        String userName = UserName_PREFIX + RandomUtil.randomString(10);
         user.setUsername(userName);
         user.setRole("0");
         user.setCreatetime(LocalDateTime.now());
@@ -109,20 +102,21 @@ public class UserServiceImpl implements UserService{
 
     /**
      * 发验证码，存储到session里
+     *
      * @param phonenum
      * @return
      */
     @Override
     public Result sendCode(String phonenum) {
-         //校验手机号格式是否正确
+        //校验手机号格式是否正确
         boolean phoneInvalid = RegexUtils.isPhoneInvalid(phonenum);
-        if(phoneInvalid){
+        if (phoneInvalid) {
             return Result.fail("手机号格式不对");   //手机号格式不对返回错误信息
         }
         String code = RandomUtil.randomString(4);
         //TODO 发送验证码给手机
         System.out.println(code);
-        stringRedisTemplate.opsForValue().set(Login_Code_Key+phonenum,code,Login_Code_TTL, TimeUnit.MINUTES);
+        stringRedisTemplate.opsForValue().set(Login_Code_Key + phonenum, code, Login_Code_TTL, TimeUnit.MINUTES);
         return Result.success("验证码已经发送");
     }
 
@@ -130,38 +124,33 @@ public class UserServiceImpl implements UserService{
     public Result loginByPsw(UserLoginDTO userLoginDTO) {
         String phonenum = userLoginDTO.getPhoneNum();
         boolean phoneInvalid = RegexUtils.isPhoneInvalid(phonenum);// 二次检验。防止改手机号登录；或者前端写死不能改
-        if(phoneInvalid==true)
-            return Result.fail("手机号不对");
+        if (phoneInvalid == true) return Result.fail("手机号不对");
 
-        User user=userMapper.getByPhone(phonenum);
+        User user = userMapper.getByPhone(phonenum);
         //是否注册过
-        if(user==null){
-            user=createUser(userLoginDTO);
+        if (user == null) {
+            user = createUser(userLoginDTO);
             return Result.success("账号注册成功，密码为刚才输入的密码,重新登陆");
         }
         String encryptPassword = DigestUtils.md5DigestAsHex((SALT + userLoginDTO.getPassword()).getBytes());
-        if(userLoginDTO.getPassword()==null||!(user.getPassword()).equals(encryptPassword)){
+        if (userLoginDTO.getPassword() == null || !(user.getPassword()).equals(encryptPassword)) {
             return Result.fail("密码不对，登陆失败");
         }
 
 
-        String token= UUID.randomUUID().toString();  //给前端发token
-        String tokenKey=Login_Password_Key+token;       //后端以hashmap形式存储到缓存，过第二层拦截器
+        String token = UUID.randomUUID().toString();  //给前端发token
+        String tokenKey = Login_Password_Key + token;       //后端以hashmap形式存储到缓存，过第二层拦截器
         UserVO userVO = BeanUtil.copyProperties(user, UserVO.class);
-        Map<String, Object> userMap = BeanUtil.beanToMap(userVO, new HashMap<>()
-                , CopyOptions.create().setIgnoreNullValue(true)
-                        .setFieldValueEditor(
-                                (name, value) -> {
-                                    if (value != null) {
-                                        return value.toString();
-                                    } else {
-                                        return null; // 或者你想要的默认值
-                                    }
-                                }
-                        ));
+        Map<String, Object> userMap = BeanUtil.beanToMap(userVO, new HashMap<>(), CopyOptions.create().setIgnoreNullValue(true).setFieldValueEditor((name, value) -> {
+            if (value != null) {
+                return value.toString();
+            } else {
+                return null; // 或者你想要的默认值
+            }
+        }));
 
-        stringRedisTemplate.opsForHash().putAll(tokenKey,userMap);
-        stringRedisTemplate.expire(tokenKey,Login_User_TTL,TimeUnit.MINUTES);
+        stringRedisTemplate.opsForHash().putAll(tokenKey, userMap);
+        stringRedisTemplate.expire(tokenKey, Login_User_TTL, TimeUnit.MINUTES);
         return Result.success(token);
 
     }
