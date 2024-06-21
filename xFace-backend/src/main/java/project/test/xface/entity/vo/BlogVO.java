512f919a01088d5e3001028690c6bce507c2364c
cybertron-loki <944980390@qq.com>
2024-06-21 19:46:10 +0800

    大小写

--- xFace-backend/src/main/java/project/test/xface/entity/vo/BlogVO.java
+++ xFace-backend/src/main/java/project/test/xface/entity/vo/BlogVO.java
@@ -1,8 +1,15 @@
 package project.test.xface.entity.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * 考虑到需要用户头像，但如果再次查询只为了一个头像不值得，所以头像干脆写里。
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BlogVO {
     private Long id;
     private Long userId;
