82c2385a13b7b36e3e9db0a89b6c3672a5fb9ae6
Cybertron-Loki <ssr865579@gmail.com>
2025-01-09 15:06:39 +0800

    Update BlogService.java

--- xFace-backend/src/main/java/project/test/xface/service/BlogService.java
+++ xFace-backend/src/main/java/project/test/xface/service/BlogService.java
@@ -8,7 +8,7 @@ import project.test.xface.entity.pojo.Comment;
 import java.util.List;
 
 /**
- * @author XiaoMing
+ *
  * @description 针对表【blog(帖子表)】的数据库操作Service
  * @createDate 2024-06-05 20:48:03
  */
