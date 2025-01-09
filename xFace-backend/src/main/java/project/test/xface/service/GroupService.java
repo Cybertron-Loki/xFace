7c18ba1a13813138af09a3ee133d63bf8e9f2d3e
Cybertron-Loki <ssr865579@gmail.com>
2025-01-09 15:07:34 +0800

    Update GroupService.java

--- xFace-backend/src/main/java/project/test/xface/service/GroupService.java
+++ xFace-backend/src/main/java/project/test/xface/service/GroupService.java
@@ -6,7 +6,7 @@ import project.test.xface.entity.pojo.Group;
 import project.test.xface.entity.pojo.Groupmember;
 
 /**
- * @author XiaoMing
+ * 
  * @description 针对表【Group(群聊信息表)】的数据库操作Service
  * @createDate 2024-06-05 20:48:03
  */
