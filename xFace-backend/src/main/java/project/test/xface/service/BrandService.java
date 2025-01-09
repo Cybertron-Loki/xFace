41bd39b1c86920c730f4b600cfd0ad4da1759c65
Cybertron-Loki <ssr865579@gmail.com>
2025-01-09 15:08:11 +0800

    Update BrandService.java

--- xFace-backend/src/main/java/project/test/xface/service/BrandService.java
+++ xFace-backend/src/main/java/project/test/xface/service/BrandService.java
@@ -5,7 +5,7 @@ import project.test.xface.entity.pojo.BlogType;
 import project.test.xface.entity.pojo.Brand;
 
 /**
- * @author XiaoMing
+ * 
  * @description 针对表【brand(商家/品牌表)】的数据库操作Service
  * @createDate 2024-06-05 20:48:03
  */
